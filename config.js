b39445f9fdf4e93920691cac5a792d34bd9dcb67
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-07-27 22:19:46 +0700

    Change dev port to 3000

--- config.js
+++ config.js
@@ -3,16 +3,18 @@ const commonConfig = {
   apiPath: 'http://staging.api.ywc15.ywc.in.th',
   facebookAppsID: '134784707110171',
   gaTrackingID: 'UA-103240510-1',
-  port: 12345,
+  port: 3000,
 };
 
 const environmentConfig = {
   development: {},
   staging: {
-    isProduction: true
+    isProduction: true,
+    port: 12345,
   },
   production: {
     isProduction: true,
+    apiPath: 'http://staging.api.ywc15.ywc.in.th',
     facebookAppsID: '715340261988670',
     gaTrackingID: 'UA-103240510-2',
     port: 12346

9b71a79a39ed669f5166ce60fce957b5639a8d3d
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-08-20 18:11:46 +0700

    Add stepper text

--- component/Register/Stepper.js
+++ component/Register/Stepper.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
-const Stepper = (props) => (
-  <h1>{props.step}</h1>
+import colors from '../../utils/colors';
+
+const Stepper = props => (
+  <div className="stepper-text-wrapper">
+    <h1 className="step-title">STEP {props.step}</h1>
+    <h2 className="step-label">ข้อมูลการติดต่อ</h2>
+    <style jsx>{`
+      .stepper-text-wrapper {
+        padding: 15px 0;
+        .step-title {
+          font-family: 'Agency FB';
+          font-weight: 600;
+          font-size: 48px;
+          line-height: 46px;
+          color: ${colors.cyan};
+          letter-spacing: 3px;
+        }
+        .step-label {
+          font-family: 'Cordia New';
+          font-size: 24px;
+          font-weight: 600;
+        }
+      }
+    `}</style>
+  </div>
 );
 
 export default Stepper;
--- pages/registration/index.js
+++ pages/registration/index.js
@@ -31,49 +31,78 @@ export default class MainRegistration extends Component {
 
   render() {
     const { proceedStepOne, proceedStepTwo, registerData } = this.props;
-    const { major, currentStep, saving } = registerData;
+    const { currentStep, saving } = registerData;
     return (
-      <div className="container">
-        <div>
-          <Stepper step={currentStep} />
-          <div className="step-wrapper">
-            <div className="step-inner-wrapper">
-              {currentStep === 1 && (
-                <StepOne {...registerData} onSubmit={() => proceedStepOne(registerData)} />
-              )}
-              {currentStep === 2 && (
-                <StepTwo {...registerData} onSubmit={() => proceedStepTwo(registerData)} />
-              )}
-            </div>
-            {saving && (
-              <div className="loader-wrapper">
-                <FullAreaLoader />
-              </div>
+      <div className="container register-container">
+        <Stepper step={currentStep} />
+        <div className="step-wrapper">
+          <div className="step-inner-wrapper">
+            {currentStep === 1 && (
+              <StepOne {...registerData} onSubmit={() => proceedStepOne(registerData)} />
             )}
-            <style jsx global>{`
-              .column {
-                padding-top: 8px;
-                padding-bottom: 0;
-                @media(max-width: 768px) {
-                  padding-top: 0.75rem;
-                }
-              }  
-            `}</style>
-            <style jsx>{`
-              .step-wrapper {
+            {currentStep === 2 && (
+              <StepTwo {...registerData} onSubmit={() => proceedStepTwo(registerData)} />
+            )}
+          </div>
+          {saving && (
+            <div className="loader-wrapper">
+              <FullAreaLoader />
+            </div>
+          )}
+          <style jsx global>{`
+            .register-container {
+              padding: 25px 0;
+            }
+            .column {
+              padding-top: 8px;
+              padding-bottom: 8px;
+              margin-bottom: 0;
+              @media(max-width: 768px) {
+                padding-top: 0.75rem;
+              }
+            }  
+          `}</style>
+          <style jsx>{`
+            .step-wrapper {
+              position: relative;
+              pointer-events: none;
+              @media(max-width: 768px) {
+                margin: 15px 20px;
+              }
+              &:before {
+                display: block;
+                content: "";
+                width: 50%;
+                height: 25%;
+                left: 0px;
+                position: absolute;
+                border-top: 4px solid ${colors.cyan};
+                border-left: 4px solid ${colors.cyan};
+              }
+              &:after {
+                display: block;
+                content: "";
+                width: 50%;
+                height: 25%;
+                position: absolute;
+                top: 0px;
+                right: 0px;
+                border-top: 4px solid ${colors.cyan};
+                border-right: 4px solid ${colors.cyan};
+              }
+              .step-inner-wrapper {
+                padding: 30px 20px;
+                border: 1px solid #83beea;
                 position: relative;
-                pointer-events: none;
-                @media(max-width: 768px) {
-                  margin: 15px 20px;
-                }
                 &:before {
                   display: block;
                   content: "";
                   width: 50%;
                   height: 25%;
-                  left: 0px;
                   position: absolute;
-                  border-top: 4px solid ${colors.cyan};
+                  bottom: 0px;
+                  left: 0px;
+                  border-bottom: 4px solid ${colors.cyan};
                   border-left: 4px solid ${colors.cyan};
                 }
                 &:after {
@@ -82,53 +111,26 @@ export default class MainRegistration extends Component {
                   width: 50%;
                   height: 25%;
                   position: absolute;
-                  top: 0px;
+                  bottom: 0px;
                   right: 0px;
-                  border-top: 4px solid ${colors.cyan};
+                  border-bottom: 4px solid ${colors.cyan};
                   border-right: 4px solid ${colors.cyan};
                 }
-                .step-inner-wrapper {
-                  padding: 20px;
-                  border: 1px solid #83beea;
-                  position: relative;
-                  &:before {
-                    display: block;
-                    content: "";
-                    width: 50%;
-                    height: 25%;
-                    position: absolute;
-                    bottom: 0px;
-                    left: 0px;
-                    border-bottom: 4px solid ${colors.cyan};
-                    border-left: 4px solid ${colors.cyan};
-                  }
-                  &:after {
-                    display: block;
-                    content: "";
-                    width: 50%;
-                    height: 25%;
-                    position: absolute;
-                    bottom: 0px;
-                    right: 0px;
-                    border-bottom: 4px solid ${colors.cyan};
-                    border-right: 4px solid ${colors.cyan};
-                  }
-                }
               }
-              .loader-wrapper {
-                position: absolute;
-                left: 0;
-                top: 0;
-                width: 100%;
-                height: 100%;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                background-color: rgba(0, 0, 0, 0.3);
-                pointer-events: auto;
-              }
-            `}</style>
-          </div>
+            }
+            .loader-wrapper {
+              position: absolute;
+              left: 0;
+              top: 0;
+              width: 100%;
+              height: 100%;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              background-color: rgba(0, 0, 0, 0.3);
+              pointer-events: auto;
+            }
+          `}</style>
         </div>
       </div>
     );

d36d68dbf88762793346341b985b2c9203295984
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-08-27 23:55:34 +0700

    Remove styled-jsx (still has some but maybe it time to go)

--- store/connect.js
+++ store/connect.js
@@ -2,18 +2,12 @@ import React from 'react';
 import withRedux from 'next-redux-wrapper';
 
 import App from '../component/App';
-import colors from '../utils/colors';
 import createStore from './createStore';
 
 export default (mapStateToProps, mapDispatchToProps) => (Comp) => {
   const PagesLayout = props => (
     <App {...props}>
       <Comp {...props} />
-      <style jsx global>{`
-        .section-header {
-          color: ${colors.cyan};
-        }
-      `}</style>
     </App>
   );
 

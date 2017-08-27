2b730157f1264017440b478e35111f9ed109f787
Kanisorn S <firstziiz.k@gmail.com>
2017-08-27 18:40:25 +0700

    sponsor

--- component/Landing/Sponsor.js
+++ component/Landing/Sponsor.js
@@ -1,11 +1,28 @@
 import React from 'react';
-
+import styled from 'styled-components'
 import SectionHeader from './SectionHeader';
 
+const SponsorLogo = styled.img`
+  width: 180px;
+  margin: 10px;
+`;
+
+const SponsorWarpper = styled.div`
+  
+`
+
 const Sponsor = () => (
-  <div>
+  <SponsorWarpper className="container">
     <SectionHeader title="Sponsor" subtitle="ผู้ใหญ่ใจดี" />
-  </div>
+    <div className="sponsors">
+      {
+        [1,2,3,4,5,6,7,8,9,10,11,12,13].map(e => (
+          <SponsorLogo src="/static/img/landing/cp-app.jpg" alt="" />
+        ))
+      }
+      
+    </div>
+  </SponsorWarpper>
 );
 
 export default Sponsor;

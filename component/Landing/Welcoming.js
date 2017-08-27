81158c11d79f436d1ca7156b7eab8917c4c6c65b
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-08-27 20:24:04 +0700

    MOBILE FIX!

--- component/Landing/Welcoming.js
+++ component/Landing/Welcoming.js
@@ -1,147 +1,184 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import colors from '../../utils/colors';
 
-const Square = ({ style }) => (
-  <div className="square" style={style}>
-    <style jsx>{`
-      .square {
-        width: 10px;
-        height: 10px;
-        background-color: ${colors.cyan};
-      }
-    `}</style>
-  </div>
-);
+const Square = styled.div`
+  width: 10px;
+  height: 10px;
+  background-color: ${colors.cyan};
+  ${props => props.extraMargin && 'margin-right: 5px;'}
+  @media(max-width: 768px) {
+    width: 7px;
+    height: 7px;
+  }
+`;
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const CampBranding = styled.div`
+  width: 600px;
+  margin: 0 auto;
+  position: relative;
+  @media(max-width: 768px) {
+    width: 330px;
+  }
+`;
+
+const CentralHexagon = styled.img.attrs({
+  src: '/static/img/landing/welcoming/central_hex.png'
+})`
+  width: 100%;
+`;
+
+const CampDetailContainer = styled.div`
+  position: absolute;
+  left: 18%;
+  top: 27%;
+  width: 64%;
+  @media(max-width: 768px) {
+    top: 23%;
+  }
+`;
+
+const CampLogo = styled.img.attrs({
+  src: '/static/img/logo.png',
+  alt: 'Young Webmaster Camp 15 Logo'
+})`
+  width: 80%;
+  @media(max-width: 768px) {
+    width: 77%;
+  }
+`;
+
+const SloganContainer = styled.div`
+  width: 85%;
+  margin: 0 auto;
+  @media(max-width: 768px) {
+    width: 90%;
+  }
+`;
+
+const SquareRow = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding-bottom: 7px;
+  @media(max-width: 768px) {
+    padding-bottom: 5px;
+  }
+`;
+
+const SloganWrapper = styled.div`
+  position: relative;
+  &:before {
+    display: block;
+    content: "";
+    width: 51%;
+    height: 10px;
+    top: -2px;
+    left: 0px;
+    position: absolute;
+    border-top: 4px solid ${colors.cyan};
+    border-left: 4px solid ${colors.cyan};
+  }
+  &:after {
+    display: block;
+    content: "";
+    width: 50%;
+    height: 10px;
+    position: absolute;
+    top: -2px;
+    right: 0px;
+    border-top: 4px solid ${colors.cyan};
+    border-right: 4px solid ${colors.cyan};
+  }
+`;
+
+const SloganInnerWrapper = styled.div`
+  padding: 5px 0px;
+  border: 1px solid ${colors.darkCyan};
+  @media(max-width: 768px) {
+    padding-top: 7px;
+  }
+  &:before {
+    display: block;
+    content: "";
+    width: 51%;
+    height: 10px;
+    position: absolute;
+    bottom: -2px;
+    left: 0px;
+    border-bottom: 4px solid ${colors.cyan};
+    border-left: 4px solid ${colors.cyan};
+  }
+  &:after {
+    display: block;
+    content: "";
+    width: 50%;
+    height: 10px;
+    position: absolute;
+    bottom: -2px;
+    right: 0px;
+    border-bottom: 4px solid ${colors.cyan};
+    border-right: 4px solid ${colors.cyan};
+  }
+`;
+
+const SloganText = styled.h1`
+  font-size: 26px;
+  letter-spacing: 1.8px;
+  @media(max-width: 768px) {
+    font-size: 15px;
+    letter-spacing: 1.5px;
+  }
+`;
+
+const CampDate = styled.h2`
+  font-size: 42px;
+  margin: 0;
+  line-height: 1em;
+  padding-top: 14px;
+  @media(max-width: 768px) {
+    font-size: 20px;
+    padding-top: 10px;
+  }
+`;
+
+const CampLocation = styled.h2`
+  font-size: 22px;
+  margin: 0;
+  @media(max-width: 768px) {
+    font-size: 13px;
+  }
+`;
 
 const Welcoming = () => (
-  <div className="welcoming-container">
-    <div className="camp-branding">
-      <img src="/static/img/landing/welcoming/central_hex.png" className="central-hex" />
-      <div className="camp-logo">
-        <img src="/static/img/logo.png" alt="YWC15" />
-        <div className="slogan-container">
-          <div className="square-container">
-            <Square style={{ marginRight: '5px' }} />
-            <Square style={{ marginRight: '5px' }} />
+  <Container>
+    <CampBranding>
+      <CentralHexagon />
+      <CampDetailContainer>
+        <CampLogo />
+        <SloganContainer className="slogan-container">
+          <SquareRow>
+            <Square extraMargin />
+            <Square extraMargin />
             <Square />
+          </SquareRow>
+          <SloganWrapper className="slogan-outer-container">
+            <SloganInnerWrapper className="slogan-inner-container">
+              <SloganText>DIGITAL INNOVATION</SloganText>
+            </SloganInnerWrapper>
+          </SloganWrapper>
+          <div>
+            <CampDate className="camp-date">4-7 JANUARY 2018</CampDate>
+            <CampLocation className="camp-location">@Assumption University (Bangna)</CampLocation>
           </div>
-          <div className="slogan-outer-container">
-            <div className="slogan-inner-container">
-              <h1>DIGITAL INNOVATION</h1>
-            </div>
-          </div>
-          <div className="info">
-            <h1 className="camp-date">4-7 JANUARY 2018</h1>
-            <h2 className="camp-location">@Assumption University (Bangna)</h2>
-          </div>
-        </div>
-      </div>
-    </div>
-    <style jsx>{`
-      .welcoming-container {
-        position: relative;
-        .camp-branding {
-          width: 45%;
-          margin: 0 auto;
-          position: relative;
-          .central-hex {
-            width: 100%;
-          }
-          .camp-logo {
-            position: absolute;
-            left: 18%;
-            top: 31%;
-            width: 64%;
-
-            img {
-              width: 60%;
-            }
-          }
-        }
-      }
-      .slogan-container {
-        {/* position: absolute; */}
-        width: 85%;
-        margin: 0 auto;
-      }
-      .square-container {
-        display: flex;
-        justify-content: flex-end;
-        padding-bottom: 7px;
-      }
-      .slogan-outer-container {
-        position: relative;
-        &:before {
-          display: block;
-          content: "";
-          width: 50%;
-          height: 10px;
-          top: -2px;
-          left: 0px;
-          position: absolute;
-          border-top: 4px solid ${colors.cyan};
-          border-left: 4px solid ${colors.cyan};
-        }
-        &:after {
-          display: block;
-          content: "";
-          width: 50%;
-          height: 10px;
-          position: absolute;
-          top: -2px;
-          right: 0px;
-          border-top: 4px solid ${colors.cyan};
-          border-right: 4px solid ${colors.cyan};
-        }
-        .slogan-inner-container {
-          padding: 5px 0px;
-          border: 1px solid ${colors.darkCyan};
-          &:before {
-            display: block;
-            content: "";
-            width: 50%;
-            height: 10px;
-            position: absolute;
-            bottom: -2px;
-            left: 0px;
-            border-bottom: 4px solid ${colors.cyan};
-            border-left: 4px solid ${colors.cyan};
-          }
-          &:after {
-            display: block;
-            content: "";
-            width: 50%;
-            height: 10px;
-            position: absolute;
-            bottom: -2px;
-            right: 0px;
-            border-bottom: 4px solid ${colors.cyan};
-            border-right: 4px solid ${colors.cyan};
-          }
-          h1 {
-            font-size: 26px;
-            letter-spacing: 1.8px;
-          }
-        }
-      }
-
-      .info {
-        .camp-date {
-          font-family: 'supermarket';
-          font-size: 42px;
-          margin: 0;
-          line-height: 1em;
-          padding-top: 14px;
-        }
-        .camp-location {
-          font-family: 'supermarket';
-          font-size: 22px;
-          margin: 0;
-        }
-      }
-    `}</style>
-  </div>
+        </SloganContainer>
+      </CampDetailContainer>
+    </CampBranding>
+  </Container>
 );
 
 export default Welcoming;

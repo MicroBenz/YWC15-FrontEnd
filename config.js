54bf5855a42d018a8f0869574f0a3efadac3a350
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2017-08-31 21:21:32 +0700

    Testing new pm2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const commonConfig = {
   isProduction: false,
-  apiPath: 'http://staging.api.ywc15.ywc.in.th',
+  apiPath: 'https://api.ywc15.ywc.in.th',
   facebookAppsID: '134784707110171',
   gaTrackingID: 'UA-103240510-1',
   port: 3000,
@@ -14,7 +14,7 @@ const environmentConfig = {
   },
   production: {
     isProduction: true,
-    apiPath: 'http://api.ywc15.ywc.in.th',
+    apiPath: 'https://api.ywc15.ywc.in.th',
     facebookAppsID: '715340261988670',
     gaTrackingID: 'UA-103240510-2',
     port: 12346

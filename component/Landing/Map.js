61fc6591693ccef22e689a0c112bb9e70bac0032
Kanisorn S <firstziiz.k@gmail.com>
2017-09-01 23:18:22 +0700

    initial map to google map

--- component/Landing/Map.js
+++ component/Landing/Map.js
@@ -1,14 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import SectionHeader from './SectionHeader';
 
-const Map = () => (
-  <div>
-    <SectionHeader title="Location" subtitle="แผนที่เดินทาง" />
-    <div className="container">
-      <img src="/static/img/landing/map_v2.png" alt="แผนที่การเดินทาง" />
-    </div>
-  </div>
-);
+const MapPhoto = styled.div`
+  transition: all .4s;
+
+  img {
+    transition: all .4s;
+  }
+  &:hover {
+    
+    img {
+      opacity: 0.6;
+    }
+  }
+`;
+
+class Map extends React.Component {
+  state = {
+    map: false
+  }
+  render() {
+    return (
+      <div>
+        <SectionHeader title="Location" subtitle="แผนที่เดินทาง" />
+        <div className="container">
+          {
+            this.state.map
+              ? (<div />)
+              : (
+                <MapPhoto>
+                  <img src="/static/img/landing/map_v2.png" alt="" />
+                </MapPhoto>
+              )
+          }
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Map;

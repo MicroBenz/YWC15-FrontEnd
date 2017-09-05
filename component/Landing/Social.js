77f5497d4367951a2a346b7e4ae0b04356353526
Kanisorn S <firstziiz.k@gmail.com>
2017-09-05 17:47:02 +0700

    increase opacity & add social section

--- component/Landing/Social.js
+++ component/Landing/Social.js
@@ -1,78 +1,65 @@
 import React from 'react';
+import styled from 'styled-components'
 
-const TwitterScript = `
-  <a
-  className="twitter-timeline"
-  data-lang="th"
-  href="https://twitter.com/ywcth"
-  >
-  Tweets by ywcth
-  </a>
-  <script
-  async
-  src="https://platform.twitter.com/widgets.js"
-  charSet="utf-8"
-  />
+const Header = styled.h1`
+  font-size: 42px;
+
+  @media(max-width: 768px) {
+    font-size: 28px;
+  }
 `;
 
-const FacebookScript = `
-  <div
-  className="fb-page"
-  data-href="https://www.facebook.com/ywcth/"
-  data-tabs="timeline"
-  data-small-header="false"
-  data-adapt-container-width="true"
-  data-hide-cover="false"
-  data-show-facepile="true"
-  >
-  <blockquote
-    cite="https://www.facebook.com/ywcth/"
-    className="fb-xfbml-parse-ignore"
-  >
-    <a href="https://www.facebook.com/ywcth/">
-      Young Webmaster Camp
-    </a>
-  </blockquote>
-  </div>
+const SocialContainer = styled.div`
+  display: flex;
+  justify-content: center;
+
+  .social {
+    display: flex;
+    opacity: 0.6;
+    transition: all .4s;
+
+    img {
+      height: 150px;
+      
+      @media(max-width: 768px) {
+        height: 75px;
+      }
+    }
+    
+    &:hover {
+      opacity: 1;
+    }
+
+    
+  }
 `;
 
 const Social = () => {
   return (
-    <div>
-      <div className="facebook">
-        <div
-          className="fb-page"
-          data-href="https://www.facebook.com/ywcth/"
-          data-tabs="timeline"
-          data-small-header="false"
-          data-adapt-container-width="true"
-          data-hide-cover="false"
-          data-show-facepile="true"
-        >
-          <blockquote
-            cite="https://www.facebook.com/ywcth/"
-            className="fb-xfbml-parse-ignore"
-          >
-            <a href="https://www.facebook.com/ywcth/">
-              Young Webmaster Camp
-            </a>
-          </blockquote>
+    <div className="container">
+      <Header>FOLLOW YWCTH ON</Header>
+      <SocialContainer>
+        <div className="social">
+          <a href="https://fb.com/ywcth">
+            <img src="/static/img/landing/materials/ywc_facebook.png" alt="" />
+          </a>
         </div>
-      </div>
-      <div className="twitter">
-        <a
-          className="twitter-timeline"
-          data-lang="th"
-          href="https://twitter.com/ywcth"
-        >
-        Tweets by ywcth
-        </a>
-        <script
-          async
-          src="://platform.twitter.com/widgets.js"
-          charSet="utf-8"
-        />
-      </div>
+        <div className="social">
+          <a href="https://twitter.com/ywcth">
+            <img src="/static/img/landing/materials/ywc_twitter.png" alt="" />
+          </a>
+        </div>
+        <div className="social">
+          <a href="https://www.instagram.com/ywcth/">
+            <img src="/static/img/landing/materials/ywc_intragram.png" alt="" />
+          </a>
+        </div>
+        <div className="social">
+          <a href="https://www.youtube.com/user/ywcth">
+            <img src="/static/img/landing/materials/ywc_youtube.png" alt="" />
+          </a>
+        </div>
+      </SocialContainer>
     </div>
   );
 };

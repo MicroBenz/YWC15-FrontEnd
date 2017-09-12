d4f40d99672d785c169f2eb1b0a0fad30656cded
Kanisorn S <firstziiz.k@gmail.com>
2017-09-12 13:27:45 +0700

    update sponsor position

--- component/Landing/Sponsor.js
+++ component/Landing/Sponsor.js
@@ -4,25 +4,47 @@ import SectionHeader from './SectionHeader';
 
 import content from './content.json'
 
-const SponsorLogo = styled.img`
-  width: 180px;
-  margin: 15px;
+const SponsorWarpper = styled.div`
+
+`
+
+const SponsorLogo = styled.div`
+  display: flex;
+
+  img {
+    max-width: 180px;
+    max-height: 180px;
+    margin: 15px;
+  }
+  order: ${props => props.desktopOrder};
+  
+  @media(max-width: 860px) {
+    flex-basis: 50%;
+    order: ${props => props.mobileOrder};
+    justify-content: ${props => props.mobileOrder % 2 === 0 ? 'flex-start' : 'flex-end'};
+  }
 `;
 
-const SponsorWarpper = styled.div`
-  
+const SponsorsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: flex-start;
+  justify-content: center;
 `
 
 const Sponsor = () => (
   <SponsorWarpper className="container">
     <SectionHeader title="Sponsor" subtitle="ผู้ใหญ่ใจดี" />
-    <div className="sponsors">
+    <SponsorsContainer>
       {
         content.sponsors.map(s => (
-          <SponsorLogo src={`/static/img/landing/sponsor/${s}.png`} alt="" />
+          <SponsorLogo desktopOrder={s.dOrder} mobileOrder={s.mOrder}>
+            <img src={`/static/img/landing/sponsor/${s.name}.png`} alt="" />
+          </SponsorLogo>
         ))
       }
-    </div>
+    </SponsorsContainer>
   </SponsorWarpper>
 );
 
